938a1002d139e35f9c3ce7a344261df1faffc743
skfaizan888 <skfaizana@gmail.com>
2025-08-24 22:14:25 +0530

    update

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,6 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), flowbiteReact(), tailwindcss()],
-  base:"/updatedUISkycouple"
+  base:"/UpdateSkyCoupleDep"
 
 });

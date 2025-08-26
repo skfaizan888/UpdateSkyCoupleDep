7bdbbe1f6077b77dced4061c18879e8c7d7c6384
skfaizan888 <skfaizana@gmail.com>
2025-08-26 17:20:57 +0530

    update code

--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,8 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import flowbiteReact from "flowbite-react/plugin/vite";
 import tailwindcss from "@tailwindcss/vite";
-// https://vite.dev/config/
-export default defineConfig({
-  base:'/UpdateSkyCoupleDep/',
-  plugins: [react(), flowbiteReact(), tailwindcss()]
 
+export default defineConfig({
+  base: '/',   // ✅ For Vercel deployment (root)
+  plugins: [react(), flowbiteReact(), tailwindcss()]
 });
